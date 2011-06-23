eed512e6b8654b4fc66fedb8ac204a0788ac2807
dmitriy.kovalev@gmail.com <dmitriy.kovalev@gmail.com@3622b9e1-0ce6-2374-50f6-d83ad264a0a2>
2011-06-23 19:04:19 +0000

    Initial commit

---
+++ trunk/org.eclipselabs.qlipse/src/org/eclipselabs/qlipse/internal/CloseQuickLookHandler.java
@@ -0,0 +1,15 @@
+package org.eclipselabs.qlipse.internal;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipselabs.qlipse.EclipseQuickLook;
+
+public class CloseQuickLookHandler extends AbstractHandler {
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        EclipseQuickLook.getDefault().closeQuickLook(HandlerUtil.getActiveWorkbenchWindow(event));
+        return null;
+    }
+}
